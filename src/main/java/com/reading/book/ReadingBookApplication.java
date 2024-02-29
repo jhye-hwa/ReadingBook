e50f47ce5704d484c64b7a0dc2a5bda1ede22df7
hyebun <hyebun96@naver.com>
2024-02-29 11:44:26 +0900

    feat: 백엔드 프로젝트 초기 환경설정 (#1)
    *feat : 백엔드 프로젝트 초기 환경 설정입니다.

---
+++ src/main/java/com/reading/book/ReadingBookApplication.java
@@ -0,0 +1,13 @@
+package com.reading.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reading/book/ReadingBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reading.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
